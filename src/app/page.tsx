cdb3ed0725b9aa95217cb618be3e45b315c1e253
Sayan67 <sayan.das.21@aot.edu.in>
2023-11-27 00:57:30 +0530

    Added the logical part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,45 @@
 "use client"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 export default function Home() {
   const [length, setLength] = useState(0);
   const [password, setPassword] = useState("");
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
+  const passwordRef = useRef(null);
+  const [copyState,setCopyState]= useState("Copy");
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqestuvwxyz";
     if (numAllowed) str += "0123456789";
     if (charAllowed) str += "!@#$%&*_-+=?/"
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
 
     }
     setPassword(pass);
 
   }, [setCharAllowed, length, numAllowed, setPassword]);
 
+  const copyPasswordToClip = useCallback(()=>{
+    if(copyState==="Copy"){
+      setCopyState("Copied!");
+      setTimeout(()=>{setCopyState("Copy")},2000);
+    }
+    window.navigator.clipboard.writeText(password);
+  },[password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllowed, charAllowed,passwordGenerator]);
+
   return (
 
     <>
-      <div className="bg-black w-full h-screen py-10">
-        <div className=" w-full max-w-md mx-auto text-center shadow-md rounded-xl px-4 py-8 text-orange-400 bg-gray-700">
+      <div className="bg-slate-600 w-full h-screen py-10">
+        <div className="gray-div bg-opacity-30 w-full max-w-md mx-auto text-center shadow-2xl rounded-xl px-4 py-8 text-orange-400 bg-white">
           <h1 className="text-white mb-4">Password Generator</h1>
           <div
             className="flex overflow-hidden mb-4">
@@ -34,41 +48,49 @@ export default function Home() {
               value={password}
               className="outline-none text-gray-600 px-3 py-1 w-full rounded-l-lg"
               placeholder="password"
-              readOnly />
+              readOnly
+              ref={passwordRef} />
             <button
-              className="outline-none bg-blue-600 rounded-r-lg text-white px-3 py-0.5 shrink-0">Copy</button>
+              className="outline-none bg-blue-600 rounded-r-lg text-white px-3 py-0.5 shrink-0"
+              onClick={
+                copyPasswordToClip}>{copyState}</button>
           </div>
           <div className="flex text-sm gap-x-2">
             <div className="flex items-center gap-x-1">
               <input type="range"
+              defaultValue={length}
                 min={6}
                 max={32}
                 value={length}
                 className="cursor-pointer"
+                onChange={(e) => { setLength(e.target.value); }}
+                
               />
               <label className="text-white">Length : {length}</label>
             </div>
             <div>
               <input type="checkbox"
 
-                defaultChecked={numAllowed}
+                defaultChecked={false}
 
                 id="numberInput"
 
                 onChange={() => {
-                  setNumAllowed((prev)=>!prev);}} />
-                  <label htmlFor="numberInput" className="ml-1">Numbers</label>
+                  setNumAllowed((prev) => !prev);
+                }} />
+              <label htmlFor="numberInput" className="ml-1">Numbers</label>
             </div>
             <div>
               <input type="checkbox"
 
-                defaultChecked={numAllowed}
+                defaultChecked={false}
 
                 id="numberInput"
 
                 onChange={() => {
-                  setNumAllowed((prevVal)=>!prevVal);}} />
-                  <label htmlFor="characterInput" className="ml-1">Symbols</label>
+                  setCharAllowed((prevVal) => !prevVal);
+                }} />
+              <label htmlFor="characterInput" className="ml-1">Symbols</label>
             </div>
           </div>
         </div>
